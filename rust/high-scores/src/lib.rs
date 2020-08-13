219ebc1f72c8eff5e2eb4489bb533efa6ba1c5ac
Dennis Merkus <dennis.merkus@pm.me>
2020-08-13 18:46:46 +0200

    Complete rust/high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        match self.scores.len() {
+            0 => None,
+            _ => Some(self.scores[self.scores.len() - 1]),
+        }
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        match self.scores.len() {
+            0 => None,
+            _ => Some(
+                self.scores
+                    .iter()
+                    .fold(0, |best, &score| if score > best { score } else { best }),
+            ),
+        }
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut scores = Vec::from(self.scores);
+        scores.sort_by(|a, b| b.cmp(a));
+
+        if scores.len() > 3 {
+            scores.resize(3, 0);
+        }
+
+        scores
+    }
+}
