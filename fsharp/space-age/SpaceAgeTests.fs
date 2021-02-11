48492c8730d9b25993453f91c2149e68cc53d265
Dennis Merkus <dennis.merkus@isatis-health.nl>
2021-02-11 12:43:30 +0100

    fsharp/space-age: Change back test specification

--- fsharp/space-age/SpaceAgeTests.fs
+++ fsharp/space-age/SpaceAgeTests.fs
@@ -2,31 +2,33 @@
 
 module SpaceAgeTests
 
+open FSharp.Data.UnitSystems.SI.UnitSymbols
+
 open FsUnit.Xunit
 open Xunit
 
 open SpaceAge
 
 [<Fact>]
-let ``Age on Earth``() = age Earth 1000000000.0<s> |> should (equalWithin 0.01) 31.69
+let ``Age on Earth``() = age Earth 1000000000L<s> |> should (equalWithin 0.01) 31.69
 
 [<Fact>]
-let ``Age on Mercury``() = age Mercury 2134835688.0<s> |> should (equalWithin 0.01) 280.88
+let ``Age on Mercury``() = age Mercury 2134835688L<s> |> should (equalWithin 0.01) 280.88
 
 [<Fact>]
-let ``Age on Venus``() = age Venus 189839836.0<s> |> should (equalWithin 0.01) 9.78
+let ``Age on Venus``() = age Venus 189839836L<s> |> should (equalWithin 0.01) 9.78
 
 [<Fact>]
-let ``Age on Mars``() = age Mars 2129871239.0<s> |> should (equalWithin 0.01) 35.88
+let ``Age on Mars``() = age Mars 2129871239L<s> |> should (equalWithin 0.01) 35.88
 
 [<Fact>]
-let ``Age on Jupiter``() = age Jupiter 901876382.0<s> |> should (equalWithin 0.01) 2.41
+let ``Age on Jupiter``() = age Jupiter 901876382L<s> |> should (equalWithin 0.01) 2.41
 
 [<Fact>]
-let ``Age on Saturn``() = age Saturn 2000000000.0<s> |> should (equalWithin 0.01) 2.15
+let ``Age on Saturn``() = age Saturn 2000000000L<s> |> should (equalWithin 0.01) 2.15
 
 [<Fact>]
-let ``Age on Uranus``() = age Uranus 1210123456.0<s> |> should (equalWithin 0.01) 0.46
+let ``Age on Uranus``() = age Uranus 1210123456L<s> |> should (equalWithin 0.01) 0.46
 
 [<Fact>]
-let ``Age on Neptune``() = age Neptune 1821023456.0<s> |> should (equalWithin 0.01) 0.35
+let ``Age on Neptune``() = age Neptune 1821023456L<s> |> should (equalWithin 0.01) 0.35
