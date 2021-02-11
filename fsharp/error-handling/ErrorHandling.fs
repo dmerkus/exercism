fcb1a5fcd971c3c3be33191b43e277b7cff33348
Dennis Merkus <dennis.merkus@isatis-health.nl>
2021-02-11 10:43:17 +0100

    Solve fsharp/error-handling

---
+++ fsharp/error-handling/ErrorHandling.fs
@@ -0,0 +1,25 @@
+﻿module ErrorHandling
+
+open System
+
+let handleErrorByThrowingException () = failwith "test"
+
+let handleErrorByReturningOption (input: string): Option<int> =
+    match Int32.TryParse input with
+    | (true, n) -> Some n
+    | (false, _) -> None
+
+let handleErrorByReturningResult (input: string) =
+    match Int32.TryParse input with
+    | (true, n) -> Ok n
+    | (false, _) -> Error "Could not convert input to integer"
+
+let bind (switchFunction: int -> Result<int, string>) = function
+    | Ok x -> switchFunction x
+    | Error e -> Error e
+
+let cleanupDisposablesWhenThrowingException (resource: IDisposable) =
+    try
+        failwith "test"
+    finally
+        resource.Dispose()
